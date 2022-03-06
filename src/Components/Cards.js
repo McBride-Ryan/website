664d8d8882e6cc5cd725a59af65dd56eaeda3e07
Ryan McBride <ryanmcbride@ryans-mbp.myfiosgateway.com>
2022-03-06 10:16:18 -0500

    adds cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -12,105 +12,84 @@ export default function Cards() {
             <div class="col-1-of-3">
                 <div class="card">
                     <div class="card__side card__side--front card__side--front-1">
-                        <div class="card__picture card__picture--1">
-                            PIC
+                        <div class="card__picture card__picture--1">      
                         </div>
-                        <img src="" class="card__svg-icon spin" alt="" />
+                        <img src="" class="card__svg-icon" alt="" />
                         <h4 class="card__heading" >
                             <span class="card__heading-span--1">
-                                Booking Microservice
+                                IMDB CLONE
                             </span>
                         </h4>
+                        <div class="card__description">
+                            <p>
+                                A fullstack movie application with authentication and TMDB API requests.
+                            </p>
+                        </div>
                         <div class="card__details">
                             <ul>
-                                <li>AWS SQS</li>
-                                <li>Docker</li>
-                                <li>NodeJS</li>
-                                <li>Cassandra</li>
+                                <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>Livewire</li>
+                                <li>MySQL</li>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="card__side card__side--back card__side--back-1">
-                        <div class="card__cta">
-                            <div class="card__box">
-                                <div class="card__description">
-                                    <p>
-                                        A back-end booking system, handles 1500+ requests / sec
-                                    </p>
-                                </div>
-                            </div>
-                            <a href="https://github.com/aj-4/airbnb-booking-microsvc" class="btn btn--white btn--github">Source Code</a>
-                        </div>
+                            <div className='card__cta'><a href="/" className='card__btn'>Source Code</a></div>
+                        </div>  
                     </div>
                 </div>
             </div>
             <div class="col-1-of-3">
                 <div class="card">
                     <div class="card__side card__side--front card__side--front-1">
-                        <div class="card__picture card__picture--1">
-                            PIC
+                        <div class="card__picture card__picture--1">      
                         </div>
-                        <img src="" class="card__svg-icon spin" alt="" />
+                        <img src="" class="card__svg-icon" alt="" />
                         <h4 class="card__heading" >
                             <span class="card__heading-span--1">
-                                Booking Microservice
+                                IMDB CLONE
                             </span>
                         </h4>
+                        <div class="card__description">
+                            <p>
+                                A fullstack movie application with authentication and TMDB API requests.
+                            </p>
+                        </div>
                         <div class="card__details">
                             <ul>
-                                <li>AWS SQS</li>
-                                <li>Docker</li>
-                                <li>NodeJS</li>
-                                <li>Cassandra</li>
+                                <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>Livewire</li>
+                                <li>MySQL</li>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="card__side card__side--back card__side--back-1">
-                        <div class="card__cta">
-                            <div class="card__box">
-                                <div class="card__description">
-                                    <p>
-                                        A back-end booking system, handles 1500+ requests / sec
-                                    </p>
-                                </div>
-                            </div>
-                            <a href="https://github.com/aj-4/airbnb-booking-microsvc" class="btn btn--white btn--github">Source Code</a>
-                        </div>
+                            <div className='card__cta'><a href="/" className='card__btn'>Source Code</a></div>
+                        </div>  
                     </div>
                 </div>
             </div>
             <div class="col-1-of-3">
                 <div class="card">
                     <div class="card__side card__side--front card__side--front-1">
-                        <div class="card__picture card__picture--1">
-                            PIC
+                        <div class="card__picture card__picture--1">      
                         </div>
-                        <img src="" class="card__svg-icon spin" alt="" />
+                        <img src="" class="card__svg-icon" alt="" />
                         <h4 class="card__heading" >
                             <span class="card__heading-span--1">
-                                Booking Microservice
+                                IMDB CLONE
                             </span>
                         </h4>
+                        <div class="card__description">
+                            <p>
+                                A fullstack movie application with authentication and TMDB API requests.
+                            </p>
+                        </div>
                         <div class="card__details">
                             <ul>
-                                <li>AWS SQS</li>
-                                <li>Docker</li>
-                                <li>NodeJS</li>
-                                <li>Cassandra</li>
+                                <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>Livewire</li>
+                                <li>MySQL</li>
                             </ul>
-                        </div>
-                    </div>
-                    <div class="card__side card__side--back card__side--back-1">
-                        <div class="card__cta">
-                            <div class="card__box">
-                                <div class="card__description">
-                                    <p>
-                                        A back-end booking system, handles 1500+ requests / sec
-                                    </p>
-                                </div>
-                            </div>
-                            <a href="https://github.com/aj-4/airbnb-booking-microsvc" class="btn btn--white btn--github">Source Code</a>
-                        </div>
+                            <div className='card__cta'><a href="/" className='card__btn'>Source Code</a></div>
+                        </div>  
                     </div>
                 </div>
             </div>
