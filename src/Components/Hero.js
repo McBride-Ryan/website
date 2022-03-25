38f9a3e0e00e097896e4374e4388e714d3e75049
Ryan McBride <ry.mcbride9@gmail.com>
2022-03-25 11:00:31 -0400

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
         <div className="header__text-box">
             <h1 className="heading-primary">
                 <span className="heading-primary--main">Ryan McBride</span>
-                <span className="heading-primary--sub">Software Developer</span>
+                <span className="heading-primary--sub">Software Engineer</span>
             </h1>
 
             <a href="#projects" className="btn btn--white btn--animated smooth-goto">See Projects</a>
