c67fd08910b0a146c389b059ee9fd7581ecd6c6b
Ryan McBride <ry.mcbride9@gmail.com>
2024-06-15 19:37:14 -0400

    adds search

--- src/views/NFL/Quarterbacks.vue
+++ src/views/NFL/Quarterbacks.vue
@@ -90,7 +90,15 @@
 
             </tbody>
         </table>
-
+        <div>
+            <label for="default-search" class="mb-2 text-xl font-medium text-gray-900 sr-only">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+                <input v-model="searchValue" type="search" id="default-search" class=" p-2 pl-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search..." required>
+            </div>
+        </div>
         <EasyDataTable
             :items="players"
             :headers="headers"
